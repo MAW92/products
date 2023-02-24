becedc430a7507c32ddcdc6b8f2c98dae26d5122
MAW92 <45414617+MAW92@users.noreply.github.com>
2023-02-24 08:35:50 -0800

    Update
    Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./css/**/*.{html,js}"],
-  theme: {
-    
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./css/**/*.{html,js}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio')],
+};
