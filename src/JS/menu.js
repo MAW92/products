158e717ae3046870774fa3d496b4fe515098921d
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-12 09:27:33 -0700

    update

---
+++ src/JS/menu.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+  var menu=document.getElementById("mobile-menu");
+  if(menu.style.display==="none") {
+    menu.style.display="block";
+    document.getElementById("menu-icon").style.display="none";
+    document.getElementById("close-icon").style.display="block";
+  } else {
+    menu.style.display="none";
+    document.getElementById("menu-icon").style.display="block";
+    document.getElementById("close-icon").style.display="none";
+  }
+}
+var x=document.getElementById("mobile-menu");
+if(x.style.display==="block") {
+  x.style.display="none";
+} else {
+  x.style.display="block";
+}
+function hideMenuOnResize() {
+  var x=document.getElementById("mobile-menu");
+  var y=window.innerWidth;
+  if(y>640) {
+    x.style.display="none";
+  }
+}
+window.addEventListener("resize",hideMenuOnResize);
+hideMenuOnResize();
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports={
 	content:
 		[
 			"./index.html",
-			"./src/populateProductCard.js"
+			"./src/JS/*.{html,js}"
 		],
 	theme: {
 		extend: {},
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/forms'),
 	],
 };
