c60cbf592c08aa79c60dd121a47351a69ef168a1
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-14 11:33:02 -0700

    update

--- src/JS/toggleView.js
+++ src/JS/toggleView.js
@@ -2,30 +2,33 @@ const toggleButton=document.getElementById("toggleButton");
 const productCardGrid=document.getElementById("productCardGrid");
 const productCardList=document.getElementById("productCardList");
 
-toggleButton.addEventListener("click",() => {
-  if(productCardGrid.classList.contains("hidden")) {
-    // Switch to grid view
-    productCardGrid.classList.remove("hidden");
-    productCardList.classList.add("hidden");
-    toggleButton.textContent="Switch to List View";
+function toggleView() {
+  toggleButton.addEventListener("click",() => {
+    if(productCardGrid.classList.contains("hidden")) {
+      // Switch to grid view
+      productCardGrid.classList.remove("hidden");
+      productCardList.classList.add("hidden");
+      toggleButton.textContent="Switch to List View";
 
-    // Ensure cards in the grid view are visible
-    const cards=document.querySelectorAll(".product-card");
-    for(let i=0;i<cards.length;i++) {
-      cards[i
-      ].classList.remove("hidden");
-    }
-  } else {
-    // Switch to list view
-    productCardGrid.classList.add("hidden");
-    productCardList.classList.remove("hidden");
-    toggleButton.textContent="Switch to Grid View";
+      // Ensure cards in the grid view are visible
+      const cards=document.querySelectorAll(".product-card");
+      for(let i=0;i<cards.length;i++) {
+        cards[i
+        ].classList.remove("hidden");
+      }
+    } else {
+      // Switch to list view
+      productCardGrid.classList.add("hidden");
+      productCardList.classList.remove("hidden");
+      toggleButton.textContent="Switch to Grid View";
 
-    // Ensure cards in the grid view are hidden
-    const cards=document.querySelectorAll(".product-card");
-    for(let i=0;i<cards.length;i++) {
-      cards[i
-      ].classList.add("hidden");
+      // Ensure cards in the grid view are hidden
+      const cards=document.querySelectorAll(".product-card");
+      for(let i=0;i<cards.length;i++) {
+        cards[i
+        ].classList.add("hidden");
+      }
     }
-  }
-});
+  });
+}
+toggleView();
