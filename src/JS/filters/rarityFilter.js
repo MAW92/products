1fa4f50b866f81aff3072a37d521632f7e23efcc
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-17 11:46:49 -0700

    update

--- src/JS/filters/rarityFilter.js
+++ src/JS/filters/rarityFilter.js
@@ -1,110 +1,89 @@
-// Fetch the filter menu element
-const rarityFilter=document.getElementById('rarityFilter');
+let cards=[];
 
-// Event listener for the filter menu
-rarityFilter.addEventListener('change',filterCardsByRarity);
+const filterButton=document.getElementById("filterButton");
+const filterMenu=document.getElementById("filterMenu");
 
-// Function to fetch card attributes and populate the filter menu
-function fetchCardAttributes() {
-  fetch('./src/cardAttributes.json')
-    .then(response => response.json())
-    .then(data => {
-      const rarities=[...new Set(data.cards.map(card => card.rarity))];
+filterButton.addEventListener("click",() => {
+  filterMenu.classList.toggle("-translate-x-full");
+});
 
-      // Populate the filter menu
-      rarities.forEach(rarity => {
-        const option=document.createElement('option');
-        option.value=rarity;
-        option.textContent=rarity;
-        rarityFilter.appendChild(option);
-      });
-    });
+const handleFilterChange=() => {
+  displayFilteredCards(filterCardsByFilters(
+    Array.from(document.querySelectorAll('input[name="attributeFilter"]:checked')).map(e => e.value),
+    Array.from(document.querySelectorAll('input[name="conditionFilter"]:checked')).map(e => e.value),
+    // Repeat the same pattern for other filters
+  ));
+};
+
+function filterCardsByFilters(attributeFilters,conditionFilters) {
+  return cards.filter(card => {
+    return (
+      (attributeFilters.length===0||attributeFilters.includes(card.attribute))&&
+      (conditionFilters.length===0||conditionFilters.includes(card.cardCondition))
+      // Repeat the same pattern for other filters
+    );
+  });
 }
 
-// Function to filter and display the cards based on rarity
-function filterCardsByRarity() {
-  fetch('./src/cardAttributes.json')
-    .then(response => response.json())
-    .then(data => {
-      const selectedRarity=rarityFilter.value;
-      const cardList=document.getElementById('cardList');
+function displayFilteredCards(filteredCards) {
+  const cardList=document.getElementById("cardList");
+  cardList.innerHTML="";
 
-      // Clear the previous card list
-      cardList.innerHTML='';
+  filteredCards.forEach(card => {
+    const productCard=document.createElement("div");
+    productCard.classList.add(
+      "border",
+      "border-zinc-50",
+      "backdrop",
+      "rounded-lg",
+      "shadow-lg",
+      "overflow-hidden",
+      "will-change-transform",
+      "hover:transform-gpu",
+      "hover:duration-300",
+      "hover:ease-linear",
+      "hover:-translate-y-1.5",
+      "hover:bg-gradient-to-tr",
+      "hover:bg-transparent",
+      "hover:from-stone-400",
+      "hover:to-neutral-100",
+      "hover:via-neutral-950",
+      "hover:text-shadow",
+      "text-white"
+    );
 
-      // Filter the cards based on the selected rarity
-      const filteredCards=selectedRarity==='all'? data.cards:data.cards.filter(card => card.rarity===selectedRarity);
-
-      // Display the filtered cards
-      filteredCards.forEach(card => {
-        const cardElement=document.createElement('div');
-        cardElement.textContent=card.cardName;
-        cardList.appendChild(cardElement);
-      });
-
-      // Fetch the products data from the local file
-      populateProductCard();
-    });
-}
-
-function populateProductCard() {
-  fetch('./src/products.json')
-    .then(response => response.json())
-    .then(products => {
-      const cardList=document.getElementById('cardList');
-
-      // Clear the previous product cards
-      cardList.innerHTML='';
-
-      // Loop through the products and create a card for each one
-      products.forEach(product => {
-        const productCard=document.createElement('div');
-        productCard.classList.add(
-          'border',
-          'border-zinc-50',
-          'backdrop',
-          'rounded-lg',
-          'shadow-lg',
-          'overflow-hidden',
-          'will-change-transform',
-          'hover:transform-gpu',
-          'hover:duration-300',
-          'hover:ease-linear',
-          'hover:-translate-y-1.5',
-          'hover:bg-gradient-to-tr',
-          'hover:bg-transparent',
-          'hover:from-stone-400',
-          'hover:to-neutral-100',
-          'hover:via-neutral-950',
-          'hover:text-shadow',
-          'text-white'
-        );
-        productCard.innerHTML=`
-          <a href="${product.viewItemURL}" title="View on eBay" class="w-full h-full">
-            <img src="${product.galleryURL}" alt="${product.title}" class="w-full h-56 object-cover object-top border-b border-b-zinc-50 rounded-t-lg">
+    productCard.innerHTML=`
+          <a href="${card.viewItemURL}" title="View on eBay" class="w-full h-full">
+            <img src="${card.galleryURL}" alt="${card.title}" class="w-full h-56 object-cover object-top border-b border-b-zinc-50 rounded-t-lg">
             <div class="p-4">
-              <h3 class="text-xl font-black mb-2">${product.title}</h3>
-              <p class="text-sm italic mb-2">${product.conditionDescription}</p>
+              <h3 class="text-xl font-black mb-2">${card.title}</h3>
+              <p class="text-sm italic mb-2">${card.conditionDescription}</p>
               <div class="flex items-center justify-between pt-5">
-                <p class="font-bold">$${product.currentPrice}</p>
-                <p class="font-bold">S&H: $${product.shippingCost}</p>
+                <p class="font-bold">$${card.currentPrice}</p>
+                <p class="font-bold">S&H: $${card.shippingCost}</p>
               </div>
             </div>
           </a>
         `;
 
-        cardList.appendChild(productCard);
-      });
-    });
+    cardList.appendChild(productCard);
+  });
 }
 
-const filterBtn=document.getElementById('filterBtn');
-const filterMenu=document.getElementById('filterMenu');
+async function fetchCardData() {
+  try {
+    const response=await fetch("./src/cardAttributes.json");
+    return await response.json();
+  } catch(error) {
+    console.log("Error fetching card data:",error);
+    return [];
+  }
+}
 
-// Toggle the visibility of the filter menu when the filter button is clicked
-filterBtn.addEventListener('click',() => {
-  filterMenu.classList.toggle('hidden');
-});
+async function initialize() {
+  const cardData=await fetchCardData();
+  cards=cardData.cards;
+  displayFilteredCards(cards);
+}
 
-// Fetch card attributes and populate the filter menu
-fetchCardAttributes();
+initialize();
