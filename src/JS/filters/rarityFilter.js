932934fdbc23447da34926563d6dfaf2101f15f7
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-17 12:40:26 -0700

    update

--- src/JS/filters/rarityFilter.js
+++ src/JS/filters/rarityFilter.js
@@ -1,31 +1,69 @@
-// Function to filter product cards by rarity
-function filterByRarity(rarity) {
-  const cardList=document.getElementById("cardList");
+// Function to populate the rarity filters
+function populateRarityFilters(cardAttributes) {
+  const rarityFilters=document.getElementById('rarityFilters');
 
-  // Clear previous card list
-  cardList.innerHTML="";
+  // Clear previous filters
+  rarityFilters.innerHTML='';
 
-  // Filter the product cards by rarity
-  const filteredCards=productCards.filter((card) => card.rarity===rarity);
+  // Get unique rarities from card attributes
+  const rarities=[...new Set(cardAttributes.map((card) => card.rarity))];
 
-  // Populate the filtered cards
-  filteredCards.forEach((card) => {
-    // Create HTML elements for each filtered card (similar to the populateProductCards function)
-    // Append the elements to the card list
+  // Create checkboxes for each rarity
+  rarities.forEach((rarity) => {
+    const label=document.createElement('label');
+    label.classList.add('block','my-1');
+
+    const checkbox=document.createElement('input');
+    checkbox.setAttribute('type','checkbox');
+    checkbox.classList.add('mr-2');
+    checkbox.addEventListener('change',() => {
+      filterByRarity(rarity,checkbox.checked);
+    });
+
+    const labelText=document.createTextNode(rarity);
+    label.append(checkbox,labelText);
+    rarityFilters.appendChild(label);
   });
 }
 
-// Event listener for rarity filter checkboxes
-const rarityCheckboxes=document.querySelectorAll('input[name="rarityFilter"]');
-rarityCheckboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change",() => {
-    const selectedRarity=checkbox.value;
+// Function to filter the product cards by rarity
+function filterByRarity(rarity,checked) {
+  const cardList=document.getElementById('cardList');
+  const cards=cardList.querySelectorAll('.bg-white');
 
-    if(checkbox.checked) {
-      filterByRarity(selectedRarity);
-    } else {
-      // If the checkbox is unchecked, restore the original card list
-      populateProductCards();
+  cards.forEach((card) => {
+    const cardRarity=card.querySelector('p:nth-child(4)').textContent.split(': ')[1];
+    if(cardRarity===rarity&&checked) {
+      card.style.display='block';
+    } else if(cardRarity===rarity&&!checked) {
+      card.style.display='none';
     }
   });
+}
+
+// Entry point
+document.addEventListener('DOMContentLoaded',async () => {
+  // Fetch the product cards and card attributes
+  const productCards=await fetchProductCards();
+  const cardAttributes=await fetchCardAttributes();
+
+  // Populate the product cards and rarity filters
+  populateProductCards(productCards);
+  populateRarityFilters(cardAttributes);
+
+  // Add event listener to the filter button
+  const filterBtn=document.getElementById('filterBtn');
+  filterBtn.addEventListener('click',toggleMenu);
 });
+
+// Function to toggle the filter menu
+function toggleMenu() {
+  const filterMenu=document.getElementById('filterMenu');
+  const menuIcon=document.getElementById('menu-icon');
+  const closeIcon=document.getElementById('close-icon');
+
+  filterMenu.classList.toggle('translate-x-0');
+  filterMenu.classList.toggle('-translate-x-full');
+  menuIcon.classList.toggle('hidden');
+  closeIcon.classList.toggle('hidden');
+}
--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -1,49 +1,60 @@
-// Fetch the products and card attributes data
-fetch('products.json')
-  .then(response => response.json())
-  .then(products => {
-    fetch('cardAttributes.json')
-      .then(response => response.json())
-      .then(cardAttributes => {
-        populateProductCards(products,cardAttributes);
-      });
-  })
-  .catch(error => console.log(error));
+// Fetch the product cards from 'products.json'
+async function fetchProductCards() {
+  try {
+    const response=await fetch('./src/products.json');
+    const data=await response.json();
+    return data;
+  } catch(error) {
+    console.error('Error fetching product cards:',error);
+    return [];
+  }
+}
 
+// Fetch the card attributes from 'cardAttributes.json'
+async function fetchCardAttributes() {
+  try {
+    const response=await fetch('./src/cardAttributes.json');
+    const data=await response.json();
+    return data;
+  } catch(error) {
+    console.error('Error fetching card attributes:',error);
+    return [];
+  }
+}
 // Function to populate the product cards
-function populateProductCards() {
-  const cardList=document.getElementById("cardList");
+function populateProductCards(productCards) {
+  const cardList=document.getElementById('cardList');
 
   // Clear previous card list
-  cardList.innerHTML="";
+  cardList.innerHTML='';
 
   // Loop through the product cards and create HTML elements
   productCards.forEach((card) => {
-    const cardItem=document.createElement("div");
-    cardItem.classList.add("bg-white","border","rounded","p-4");
+    const cardItem=document.createElement('div');
+    cardItem.classList.add('bg-white','border','rounded','p-4');
 
-    const cardImage=document.createElement("img");
-    cardImage.setAttribute("src",card.imageUrl);
-    cardImage.setAttribute("alt",card.name);
-    cardImage.classList.add("mx-auto","h-40","w-40","object-cover");
+    const cardImage=document.createElement('img');
+    cardImage.setAttribute('src',card.imageUrl);
+    cardImage.setAttribute('alt',card.name);
+    cardImage.classList.add('mx-auto','h-40','w-40','object-cover');
 
-    const cardName=document.createElement("h2");
-    cardName.classList.add("text-xl","font-bold","mt-2");
+    const cardName=document.createElement('h2');
+    cardName.classList.add('text-xl','font-bold','mt-2');
     cardName.textContent=card.name;
 
-    const cardDetails=document.createElement("div");
-    cardDetails.classList.add("mt-2");
+    const cardDetails=document.createElement('div');
+    cardDetails.classList.add('mt-2');
 
-    const game=document.createElement("p");
+    const game=document.createElement('p');
     game.textContent=`Game: ${card.game}`;
 
-    const attribute=document.createElement("p");
+    const attribute=document.createElement('p');
     attribute.textContent=`Attribute: ${card.attribute}`;
 
-    const condition=document.createElement("p");
+    const condition=document.createElement('p');
     condition.textContent=`Condition: ${card.condition}`;
 
-    const rarity=document.createElement("p");
+    const rarity=document.createElement('p');
     rarity.textContent=`Rarity: ${card.rarity}`;
 
     // Append elements to the card item
@@ -52,6 +63,4 @@ function populateProductCards() {
     cardList.appendChild(cardItem);
   });
 }
-
-// Call the function to populate the initial card list
 populateProductCards();
