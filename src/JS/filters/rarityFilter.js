60e44a8d4f8c5041252067e752e8e06ea9522d78
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-17 23:53:19 -0700

    update

--- src/JS/filters/rarityFilter.js
+++ src/JS/filters/rarityFilter.js
@@ -1,68 +1,76 @@
-// Fetch the card attributes from 'cardAttributes.json'
-async function fetchCardAttributes() {
-  try {
-    const response=await fetch('./src/cardAttributes.json');
-    const data=await response.json();
-    return data;
-  } catch(error) {
-    console.error('Error fetching card attributes:',error);
-    return [];
-  }
-}
+function fetchFilterOptions() {
+  return fetch('./src/cardAttributes.json')
+    .then(response => response.json())
+    .then(data => {
+      const filterOptions={
+        games: [],
+        rarities: [],
+        features: [],
+        conditions: [],
+        attributes: []
+      };
 
-// Get the rarity filter element
-const rarityFilter=document.getElementById('rarityFilter');
+      // Extract unique filter options from the JSON data
+      data.forEach(card => {
+        if(!filterOptions.games.includes(card.Game)) {
+          filterOptions.games.push(card.Game);
+        }
+        if(!filterOptions.rarities.includes(card.Rarity)) {
+          filterOptions.rarities.push(card.Rarity);
+        }
+        if(!filterOptions.features.includes(card.Features)) {
+          filterOptions.features.push(card.Features);
+        }
+        if(!filterOptions.conditions.includes(card.cardCondition)) {
+          filterOptions.conditions.push(card.cardCondition);
+        }
+        if(card.Attribute&&!filterOptions.attributes.includes(card.Attribute)) {
+          filterOptions.attributes.push(card.Attribute);
+        }
+      });
 
-// Populate the filter menu
-fetchCardAttributes()
-  .then((data) => {
-    const rarities=data.rarities;
+      // Populate the filter options in the HTML select elements
+      const gameFilterSelect=document.getElementById('gameFilter');
+      filterOptions.games.forEach(game => {
+        const option=document.createElement('option');
+        option.value=game;
+        option.textContent=game;
+        gameFilterSelect.appendChild(option);
+      });
 
-    rarities.forEach(rarity => {
-      const option=document.createElement('option');
-      option.value=rarity;
-      option.textContent=rarity;
-      rarityFilter.appendChild(option);
+      const rarityFilterSelect=document.getElementById('rarityFilter');
+      filterOptions.rarities.forEach(rarity => {
+        const option=document.createElement('option');
+        option.value=rarity;
+        option.textContent=rarity;
+        rarityFilterSelect.appendChild(option);
+      });
+
+      const featuresFilterSelect=document.getElementById('featuresFilter');
+      filterOptions.features.forEach(feature => {
+        const option=document.createElement('option');
+        option.value=feature;
+        option.textContent=feature;
+        featuresFilterSelect.appendChild(option);
+      });
+
+      const conditionFilterSelect=document.getElementById('conditionFilter');
+      filterOptions.conditions.forEach(condition => {
+        const option=document.createElement('option');
+        option.value=condition;
+        option.textContent=condition;
+        conditionFilterSelect.appendChild(option);
+      });
+
+      const attributeFilterSelect=document.getElementById('attributeFilter');
+      filterOptions.attributes.forEach(attribute => {
+        const option=document.createElement('option');
+        option.value=attribute;
+        option.textContent=attribute;
+        attributeFilterSelect.appendChild(option);
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching filter options:',error);
     });
-  })
-
-// Event listener for the filter menu
-rarityFilter.addEventListener('change',filterCardsByRarity);
-
-// Function to filter and display the cards based on rarity
-function filterCardsByRarity() {
-  const selectedRarity=rarityFilter.value;
-  const cardList=document.getElementById('cardList');
-
-  // Clear the previous card list
-  cardList.innerHTML='';
-
-  // Filter the cards based on the selected rarity
-  const filteredCards=selectedRarity==='all'? cards:cards.filter(card => card.rarity===selectedRarity);
-
-  // Display the filtered cards
-  filteredCards.forEach(card => {
-    const cardElement=document.createElement('div');
-    cardElement.textContent=card.cardName;
-    cardList.appendChild(cardElement);
-  });
 }
-
-// Get the filter button and filter menu elements
-const filterBtn=document.getElementById('filterBtn');
-const filterMenu=document.getElementById('filterMenu');
-
-// Toggle the visibility of the filter menu when the filter button is clicked
-filterBtn.addEventListener('click',() => {
-  filterMenu.classList.toggle('hidden');
-});
-
-// Function to open the filter menu
-function openFilterMenu() {
-  filterBtn.addEventListener('click',() => {
-    filterMenu.classList.remove('-translate-x-full');
-  });
-}
-
-// Call the openFilterMenu function
-openFilterMenu();
