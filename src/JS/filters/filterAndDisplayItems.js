ff720819589f5cbda421cc43dec823acf5256b6b
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-18 10:51:48 -0700

    Update filterAndDisplayItems.js

--- src/JS/filters/filterAndDisplayItems.js
+++ src/JS/filters/filterAndDisplayItems.js
@@ -1,32 +1,64 @@
-const filterAndDisplayItems=() => {
-  // Get selected filter options
-  const selectedGame=document.getElementById('gameFilter').value;
-  const selectedRarity=document.getElementById('rarityFilter').value;
-  const selectedFeatures=document.getElementById('featuresFilter').value;
-  const selectedCondition=document.getElementById('conditionFilter').value;
-  const selectedAttribute=document.getElementById('attributeFilter').value;
+// Function to fetch card data from cardAttributes.json
+async function fetchCardData() {
+  try {
+    const response=await fetch('cardAttributes.json');
+    const data=await response.json();
+    return data;
+  } catch(error) {
+    console.log('Error fetching card data:',error);
+    return [];
+  }
+}
 
-  // Filter items based on selected options
-  const filteredItems=cardItems.filter((card) => {
-    const gameMatch=selectedGame==='all'||card.game.toLowerCase().replace(/\s/g,'')===selectedGame;
-    const rarityMatch=selectedRarity==='all'||card.rarity.toLowerCase().replace(/\s/g,'')===selectedRarity;
-    const featuresMatch=selectedFeatures==='all'||card.features.toLowerCase().replace(/\s/g,'')===selectedFeatures;
-    const conditionMatch=selectedCondition==='all'||card.cardCondition.toLowerCase().replace(/\s/g,'')===selectedCondition;
-    const attributeMatch=selectedAttribute==='all'||card.attribute.toLowerCase().replace(/\s/g,'')===selectedAttribute;
-    return gameMatch&&rarityMatch&&featuresMatch&&conditionMatch&&attributeMatch;
+// Function to filter and display card items
+function filterAndDisplayItems() {
+  const gameFilter=document.getElementById('gameFilter').value;
+  const rarityFilter=document.getElementById('rarityFilter').value;
+  const featuresFilter=document.getElementById('featuresFilter').value;
+  const conditionFilter=document.getElementById('conditionFilter').value;
+  const attributeFilter=document.getElementById('attributeFilter').value;
+
+  // Filter the card items based on the selected filters
+  const filteredItems=cardItems.filter(item => {
+    return (
+      (gameFilter==='all'||item.game===gameFilter)&&
+      (rarityFilter==='all'||item.rarity.toLowerCase()===rarityFilter.toLowerCase())&&
+      (featuresFilter==='all'||item.features.toLowerCase()===featuresFilter.toLowerCase())&&
+      (conditionFilter==='all'||item.cardCondition.toLowerCase()===conditionFilter.toLowerCase())&&
+      (attributeFilter==='all'||item.attribute.toLowerCase()===attributeFilter.toLowerCase())
+    );
   });
 
-  // Display filtered items on the screen
-  const resultsContainer=document.getElementById('resultsContainer');
-  resultsContainer.innerHTML='';
+  // Display the filtered items on the screen (replace with your own code)
+  const resultContainer=document.getElementById('resultContainer');
+  resultContainer.innerHTML=''; // Clear previous results
 
-  filteredItems.forEach((card) => {
-    const cardElement=document.createElement('div');
-    cardElement.textContent=card.title;
-    resultsContainer.appendChild(cardElement);
-  });
-};
+  if(filteredItems.length>0) {
+    filteredItems.forEach(item => {
+      const cardElement=document.createElement('div');
+      cardElement.textContent=item.title;
+      resultContainer.appendChild(cardElement);
+    });
+  } else {
+    const noResultsElement=document.createElement('div');
+    noResultsElement.textContent='No results found.';
+    resultContainer.appendChild(noResultsElement);
+  }
+}
 
-// Add event listener to the "Save" button
+// Event listener for the "Save" button
 const saveButton=document.getElementById('saveButton');
 saveButton.addEventListener('click',filterAndDisplayItems);
+
+// Fetch the card data and then initialize the filtering
+fetchCardData()
+  .then(data => {
+    // Replace the cardItems array with the fetched data
+    const cardItems=data;
+
+    // Initialize the filtering on page load
+    filterAndDisplayItems();
+  })
+  .catch(error => {
+    console.log('Error fetching card data:',error);
+  });
