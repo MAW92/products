f779232425b98f36ccff40d79d5c977cf428ae70
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-18 10:34:48 -0700

    update

--- src/JS/filters/rarityFilter.js
+++ src/JS/filters/rarityFilter.js
@@ -19,34 +19,44 @@ const fetchFilterOptions=async () => {
     // Populate options
     data.forEach((card) => {
       // Populate game filter options
-      const gameOption=document.createElement('option');
-      gameOption.value=card.Game.toLowerCase().replace(/\s/g,'');
-      gameOption.textContent=card.Game;
-      gameFilterSelect.appendChild(gameOption);
+      if(card.game) {
+        const gameOption=document.createElement('option');
+        gameOption.value=card.game.toLowerCase().replace(/\s/g,'');
+        gameOption.textContent=card.game;
+        gameFilterSelect.appendChild(gameOption);
+      }
 
       // Populate rarity filter options
-      const rarityOption=document.createElement('option');
-      rarityOption.value=card.Rarity.toLowerCase().replace(/\s/g,'');
-      rarityOption.textContent=card.Rarity;
-      rarityFilterSelect.appendChild(rarityOption);
+      if(card.rarity) {
+        const rarityOption=document.createElement('option');
+        rarityOption.value=card.rarity.toLowerCase().replace(/\s/g,'');
+        rarityOption.textContent=card.rarity;
+        rarityFilterSelect.appendChild(rarityOption);
+      }
 
       // Populate features filter options
-      const featuresOption=document.createElement('option');
-      featuresOption.value=card.Features.toLowerCase().replace(/\s/g,'');
-      featuresOption.textContent=card.Features;
-      featuresFilterSelect.appendChild(featuresOption);
+      if(card.features) {
+        const featuresOption=document.createElement('option');
+        featuresOption.value=card.features.toLowerCase().replace(/\s/g,'');
+        featuresOption.textContent=card.features;
+        featuresFilterSelect.appendChild(featuresOption);
+      }
 
       // Populate condition filter options
-      const conditionOption=document.createElement('option');
-      conditionOption.value=card.cardCondition.toLowerCase().replace(/\s/g,'');
-      conditionOption.textContent=card.cardCondition;
-      conditionFilterSelect.appendChild(conditionOption);
+      if(card.cardCondition) {
+        const conditionOption=document.createElement('option');
+        conditionOption.value=card.cardCondition.toLowerCase().replace(/\s/g,'');
+        conditionOption.textContent=card.cardCondition;
+        conditionFilterSelect.appendChild(conditionOption);
+      }
 
       // Populate attribute filter options
-      const attributeOption=document.createElement('option');
-      attributeOption.value=card.Attribute? card.Attribute.toLowerCase().replace(/\s/g,''):'all';
-      attributeOption.textContent=card.Attribute||'All Attributes';
-      attributeFilterSelect.appendChild(attributeOption);
+      if(card.attribute) {
+        const attributeOption=document.createElement('option');
+        attributeOption.value=card.attribute.toLowerCase().replace(/\s/g,'');
+        attributeOption.textContent=card.attribute;
+        attributeFilterSelect.appendChild(attributeOption);
+      }
     });
   } catch(error) {
     console.error('Error fetching filter options:',error);
@@ -55,3 +65,36 @@ const fetchFilterOptions=async () => {
 
 // Call the fetchFilterOptions function to fetch and populate the filter options
 fetchFilterOptions();
+
+const filterAndDisplayItems=() => {
+  // Get selected filter options
+  const selectedGame=document.getElementById('gameFilter').value;
+  const selectedRarity=document.getElementById('rarityFilter').value;
+  const selectedFeatures=document.getElementById('featuresFilter').value;
+  const selectedCondition=document.getElementById('conditionFilter').value;
+  const selectedAttribute=document.getElementById('attributeFilter').value;
+
+  // Filter items based on selected options
+  const filteredItems=cardItems.filter((card) => {
+    const gameMatch=selectedGame==='all'||card.game.toLowerCase().replace(/\s/g,'')===selectedGame;
+    const rarityMatch=selectedRarity==='all'||card.rarity.toLowerCase().replace(/\s/g,'')===selectedRarity;
+    const featuresMatch=selectedFeatures==='all'||card.features.toLowerCase().replace(/\s/g,'')===selectedFeatures;
+    const conditionMatch=selectedCondition==='all'||card.cardCondition.toLowerCase().replace(/\s/g,'')===selectedCondition;
+    const attributeMatch=selectedAttribute==='all'||card.attribute.toLowerCase().replace(/\s/g,'')===selectedAttribute;
+    return gameMatch&&rarityMatch&&featuresMatch&&conditionMatch&&attributeMatch;
+  });
+
+  // Display filtered items on the screen
+  const resultsContainer=document.getElementById('resultsContainer');
+  resultsContainer.innerHTML='';
+
+  filteredItems.forEach((card) => {
+    const cardElement=document.createElement('div');
+    cardElement.textContent=card.title;
+    resultsContainer.appendChild(cardElement);
+  });
+};
+
+// Add event listener to the "Save" button
+const saveButton=document.getElementById('saveButton');
+saveButton.addEventListener('click',filterAndDisplayItems);
