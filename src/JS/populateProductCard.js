30b1be26f83fdaf77c651454c52e89a86c51602a
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-23 22:29:28 -0700

    update

--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -26,10 +26,10 @@ function createProductGrid() {
          class="mt-2 hover:text-shadow text-white">
         <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
           <img src="${product.galleryURL}"
-               alt="${product.Title}"
+               alt="${product.title}"
                class="w-full h-72 object-cover object-top rounded-t-lg">
           <div class="p-2 flex-wrap">
-            <h3 class="text-lg font-black text-shadow text-white">${product.Title}</h3>
+            <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
             <p class="mt-2 text-shadow font-bold text-white">${product.Price}</p>
             <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
             <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and
