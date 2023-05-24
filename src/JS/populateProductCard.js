6ce0e83ca817407ff80e1e585126e326f7cdd4b8
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-23 22:36:32 -0700

    Update populateProductCard.js

--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -19,19 +19,19 @@ function createProductGrid() {
             const productCard=document.createElement("div");
             productCard.classList.add("w-fit","mx-auto");
             productCard.innerHTML=`
-            <a href="${product.viewItemURL}"
+            <a href="${products.viewItemURL}"
          target="_blank"
          title="View on eBay"
          class="mt-2 hover:text-shadow text-white">
         <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
-          <img src="${product.galleryURL}"
-               alt="${product.title}"
+          <img src="${products.galleryURL}"
+               alt="${products.title}"
                class="w-full h-72 object-cover object-top rounded-t-lg">
           <div class="p-2 flex-wrap">
-            <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
-            <p class="mt-2 text-shadow font-bold text-white">${product.Price}</p>
-            <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
-            <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and
+            <h3 class="text-lg font-black text-shadow text-white">${products.title}</h3>
+            <p class="mt-2 text-shadow font-bold text-white">${products.Price}</p>
+            <p class="text-shadow text-sm font-bold text-white">Shipping: $${products.Shipping}</p>
+            <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${products.Condition} condition, kept sleeved and
               stored in a safe environment.</p>
           </div>
         </div>
