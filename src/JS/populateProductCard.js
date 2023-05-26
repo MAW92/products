91eebe8d02bb7c7dc19fdb630e6105d4dffeb2a1
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-26 14:48:51 -0700

    Update populateProductCard.js

--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -14,41 +14,39 @@ function createProductGrid() {
 
       // Process the combined data or perform any other operations
       console.log(products);
+
+      // Loop through the combined products array
+      for(let i=0;i<products.length;i++) {
+        const product=products[i];
+
+        // Create the product card element
+        const productCard=document.createElement("div");
+        productCard.classList.add("w-fit","mx-auto");
+        productCard.innerHTML=`
+          <a href="${product.viewItemURL}"
+            target="_blank"
+            title="View on eBay"
+            class="mt-2 hover:text-shadow text-white">
+            <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
+              <img src="${product.galleryURL}"
+                alt="${product.title}"
+                class="w-full h-72 object-cover object-top rounded-t-lg">
+              <div class="p-2 flex-wrap">
+                <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
+                <p class="mt-2 text-shadow font-bold text-white">${product.Price}</p>
+                <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
+                <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and stored in a safe environment.</p>
+              </div>
+            </div>
+          </a>`;
+
+        // Append the product card to the grid container
+        gridContainer.appendChild(productCard);
+      }
     })
     .catch(error => {
       console.log("Error fetching cardData:",error);
     });
-
-  // Loop through the combined products array
-  for(let i=0;i<products.length;i++) {
-    const product=products[i];
-
-    // Create the product card element
-    const productCard=document.createElement("div");
-    productCard.classList.add("w-fit","mx-auto");
-    productCard.innerHTML=`
-            <a href="${product.viewItemURL}"
-         target="_blank"
-         title="View on eBay"
-         class="mt-2 hover:text-shadow text-white">
-        <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
-          <img src="${product.galleryURL}"
-               alt="${product.title}"
-               class="w-full h-72 object-cover object-top rounded-t-lg">
-          <div class="p-2 flex-wrap">
-            <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
-            <p class="mt-2 text-shadow font-bold text-white">${product.Price}</p>
-            <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
-            <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and
-              stored in a safe environment.</p>
-          </div>
-        </div>
-      </a>`;
-
-    // Append the product card to the grid container
-    gridContainer.appendChild(productCard);
-  }
-
 }
 
 // Call the function to create the product grid
