9f42d81499057f9ae6ae59d3585d11b6c155ad29
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-28 10:50:22 -0700

    update

--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -5,12 +5,12 @@ async function fetchProducts() {
     const data=await response.json();
     // Convert the fetched data into a list of products
     const products=data.map(({
-      viewItemURL: listingData.viewItemURL,
-      galleryURL: listingData.galleryURL,
-      title: listingData.title,
-      Price: cardData.Price,
-      Shipping: cardData.Shipping,
-      Condition: cardData.Condition
+      viewItemURL: data.viewItemURL,
+      galleryURL: data.galleryURL,
+      Title: data.Title,
+      Price: data.Price,
+      Shipping: data.Shipping,
+      Condition: data.Condition
     }));
 
     // Display the product cards
@@ -21,17 +21,17 @@ async function fetchProducts() {
 }
 
 // Function to create product cards
-function createProductCard(product) {
+function createProductCard(products) {
   const card=document.createElement("div");
   card.innerHTML=`
-        <a href="${product.viewItemURL}" target="_blank" title="View on eBay" class="mt-2 hover:text-shadow text-white">
+        <a href="${products.viewItemURL}" target="_blank" title="View on eBay" class="mt-2 hover:text-shadow text-white">
           <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
-            <img src="${product.galleryURL}" alt="${product.title}" class="w-full h-72 object-cover object-top rounded-t-lg">
+            <img src="${products.galleryURL}" alt="${products.Title}" class="w-full h-72 object-cover object-top rounded-t-lg">
               <div class="p-2 flex-wrap">
-                <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
-                <p class="mt-2 text-shadow font-bold text-white">${product.Price}</p>
-                <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
-                <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and stored in a safe environment.</p>
+                <h3 class="text-lg font-black text-shadow text-white">${products.Title}</h3>
+                <p class="mt-2 text-shadow font-bold text-white">${products.Price}</p>
+                <p class="text-shadow text-sm font-bold text-white">Shipping: $${products.Shipping}</p>
+                <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${products.Condition} condition, kept sleeved and stored in a safe environment.</p>
               </div>
           </div>
         </a>
