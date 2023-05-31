4ad4955192217ed3c8ed8f2a3e4d837b87979142
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-05-30 19:07:14 -0700

    update

--- src/JS/filterAndDisplayItems.js
+++ src/JS/filterAndDisplayItems.js
@@ -8,53 +8,58 @@ async function fetchProducts() {
     const listingData=data.listingData;
 
     // Convert the fetched data into a list of products
-    const products=listingData.map((productData) => ({
+    products=listingData.map((productData) => ({
       viewItemURL: productData.viewItemURL,
       galleryURL: productData.galleryURL,
       title: productData.Title,
       Price: productData.Price,
       Shipping: productData.Shipping,
-      Condition: productData.Condition
+      Condition: productData.Condition,
+      Rarity: productData.Rarity,
+      Printing: productData.Printing
     }));
 
     // Display the product cards
     displayProductCards(products);
+
+    // Populate the filter menu
+    populateFilterMenu();
   } catch(error) {
     console.log("Error fetching products:",error);
   }
 }
 
-// Function to create product cards
-function createProductCard(product) {
-  const card=document.createElement("div");
-  card.innerHTML=`
-    <a href="${product.viewItemURL}" target="_blank" title="View on eBay" class="mt-2 hover:text-shadow text-white">
-      <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
-        <img src="${product.galleryURL}" alt="${product.title}" class="w-full h-72 object-cover object-top rounded-t-lg">
-        <div class="p-2 flex-wrap">
-          <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
-          <p class="mt-2 text-shadow font-bold text-white">$${product.Price}</p>
-          <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
-          <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and stored in a safe environment.</p>
-        </div>
-      </div>
-    </a>
-  `;
-  return card;
-}
+// Function to populate the filter menu
+function populateFilterMenu() {
+  const rarityFilter=document.getElementById('rarityFilter');
+  const conditionFilter=document.getElementById('conditionFilter');
+  const printingFilter=document.getElementById('printingFilter');
 
-// Function to display product cards
-function displayProductCards(products) {
-  // Get the product grid element
-  const productGrid=document.getElementById("productGrid");
+  // Get unique values for each filter option
+  const uniqueRarities=[...new Set(products.map(product => product.Rarity))];
+  const uniqueConditions=[...new Set(products.map(product => product.Condition))];
+  const uniquePrintings=[...new Set(products.map(product => product.Printing))];
 
-  // Clear previous results
-  productGrid.innerHTML='';
+  // Add options to the filter menu
+  uniqueRarities.forEach(rarity => {
+    const option=document.createElement('option');
+    option.value=rarity;
+    option.textContent=rarity;
+    rarityFilter.appendChild(option);
+  });
 
-  // Generate product cards and append to the grid
-  products.forEach((product) => {
-    const card=createProductCard(product);
-    productGrid.appendChild(card);
+  uniqueConditions.forEach(condition => {
+    const option=document.createElement('option');
+    option.value=condition;
+    option.textContent=condition;
+    conditionFilter.appendChild(option);
+  });
+
+  uniquePrintings.forEach(printing => {
+    const option=document.createElement('option');
+    option.value=printing;
+    option.textContent=printing;
+    printingFilter.appendChild(option);
   });
 }
 
@@ -95,13 +100,8 @@ function filterProducts() {
 }
 
 // Event listeners for the filter menu
-const conditionFilter=document.getElementById('conditionFilter');
-const rarityFilter=document.getElementById('rarityFilter');
-const printingFilter=document.getElementById('printingFilter');
-
-conditionFilter.addEventListener('change',filterProducts);
-rarityFilter.addEventListener('change',filterProducts);
-printingFilter.addEventListener('change',filterProducts);
+const applyFilterBtn=document.getElementById('applyFilterBtn');
+applyFilterBtn.addEventListener('click',filterProducts);
 
 // Fetch the products and display the cards
 fetchProducts();
