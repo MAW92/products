3943994aa77a03e46011dfdbfcb96c381fd6b33e
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-03 13:38:08 -0700

    update

--- src/JS/filterAndDisplayItems.js
+++ src/JS/filterAndDisplayItems.js
@@ -6,14 +6,14 @@ function filterAndDisplayItems() {
   var printingFilter=document.getElementById("printingFilter").value;
 
   // Get all product cards
-  var productCards=document.querySelectorAll("#productGrid .product-card");
+  var productCards=document.querySelectorAll("#productGrid .card");
 
   // Loop through each product card
   productCards.forEach(function (card) {
     // Get the data attributes of the card
-    var rarity=card.getAttribute("data-rarity");
-    var condition=card.getAttribute("data-condition");
-    var printing=card.getAttribute("data-printing");
+    var rarity=card.getAttribute("rarityFilter");
+    var condition=card.getAttribute("conditionFilter");
+    var printing=card.getAttribute("printingFilter");
 
     // Check if the card matches the selected filters
     var isRarityMatch=rarityFilter==="All"||rarity===rarityFilter;
@@ -32,3 +32,33 @@ document.getElementById("applyFilterBtn").addEventListener("click",filterAndDisp
 filterAndDisplayItems();
 // Call the filter and display function when the filter button is clicked
 
+const filterMenu=document.getElementById('filterMenu');
+const applyFilterBtn=document.getElementById('applyFilterBtn');
+const closeFilterBtn=document.getElementById('closeFilterBtn');
+const updateBtn=document.getElementById('updateBtn');
+const rarityFilter=document.getElementById('rarityFilter');
+const printingFilter=document.getElementById('printingFilter');
+const conditionFilter=document.getElementById('conditionFilter');
+filterBtn.addEventListener('click',() => {
+  filterMenu.classList.remove('-translate-x-full');
+});
+closeFilterBtn.addEventListener('click',() => {
+  filterMenu.classList.add('-translate-x-full');
+});
+updateBtn.addEventListener('click',() => {
+  const rarity=rarityFilter.value;
+  const printing=printingFilter.value;
+  const condition=conditionFilter.value;
+  console.log('Filters:',{ rarity,printing,condition });
+  filterMenu.classList.add('-translate-x-full');
+});
+rarityFilter.addEventListener('change',applyFilters);
+printingFilter.addEventListener('change',applyFilters);
+conditionFilter.addEventListener('change',applyFilters);
+function applyFilters() {
+  const rarity=rarityFilter.value;
+  const printing=printingFilter.value;
+  const condition=conditionFilter.value;
+
+  console.log('Filters:',{ rarity,printing,condition });
+}
