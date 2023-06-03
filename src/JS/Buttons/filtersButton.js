26a26b77b4536a844a3d6dbfac2e629698f8b77c
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-03 14:04:28 -0700

    update

--- src/JS/Buttons/filtersButton.js
+++ src/JS/Buttons/filtersButton.js
@@ -5,8 +5,12 @@ const updateBtn=document.getElementById('updateBtn');
 const rarityFilter=document.getElementById('rarityFilter');
 const printingFilter=document.getElementById('printingFilter');
 const conditionFilter=document.getElementById('conditionFilter');
-
-
+filterBtn.addEventListener('click',() => {
+  filterMenu.classList.remove('-translate-x-full');
+});
+closeFilterBtn.addEventListener('click',() => {
+  filterMenu.classList.add('-translate-x-full');
+});
 updateBtn.addEventListener('click',() => {
   const rarity=rarityFilter.value;
   const printing=printingFilter.value;
@@ -14,7 +18,9 @@ updateBtn.addEventListener('click',() => {
   console.log('Filters:',{ rarity,printing,condition });
   filterMenu.classList.add('-translate-x-full');
 });
-
+rarityFilter.addEventListener('change',applyFilters);
+printingFilter.addEventListener('change',applyFilters);
+conditionFilter.addEventListener('change',applyFilters);
 function applyFilters() {
   const rarity=rarityFilter.value;
   const printing=printingFilter.value;
@@ -37,10 +43,6 @@ function closeFilterMenu() {
 
 // Event listener for opening the filter menu when the filter button is clicked
 filterBtn.addEventListener('click',openFilterMenu);
-rarityFilter.addEventListener('change',applyFilters);
-printingFilter.addEventListener('change',applyFilters);
-conditionFilter.addEventListener('change',applyFilters);
+
 // Event listener for closing the filter menu when the close button is clicked
 closeFilterBtn.addEventListener('click',closeFilterMenu);
-filterBtn.addEventListener('click',() => { filterMenu.classList.remove('-translate-x-full'); });
-closeFilterBtn.addEventListener('click',() => { filterMenu.classList.add('-translate-x-full'); });
