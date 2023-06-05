cf13c1a1c937918097926e44aade1cded1df5928
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-05 16:17:48 -0700

    Update filterAndDisplayItems.js

--- src/JS/filterAndDisplayItems.js
+++ src/JS/filterAndDisplayItems.js
@@ -28,7 +28,30 @@ function filterAndDisplayItems() {
     }
   });
 }
-filterAndDisplayItems();
-// Call the filter and display function when the filter button is clicked
-document.getElementById("applyButton").addEventListener("click",filterAndDisplayItems());
 
+// Call the filter and display function when the filter button is clicked
+document.getElementById("applyButton").addEventListener("click",filterAndDisplayItems);
+
+// Function to display product cards
+function displayProductCards(products) {
+  // Get the product grid element
+  const productGrid=document.getElementById("productGrid");
+  const productCardList=document.getElementById("productCardList");
+
+  // Clear previous results
+  productGrid.innerHTML="";
+  productCardList.innerHTML="";
+
+  // Generate product cards and append to the grid
+  products.forEach(product => {
+    const card=createProductCard(product);
+    productGrid.appendChild(card);
+    productCardList.appendChild(card.cloneNode(true));
+  });
+
+  // Call the filter and display function after updating the cards
+  filterAndDisplayItems();
+}
+
+// Call the fetch function initially
+fetchProducts();
