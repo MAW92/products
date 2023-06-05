2f5dddc3105d480ad255d7ecff9eec6758431db5
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-05 15:44:46 -0700

    Update toggleView.js

--- src/JS/Buttons/toggleView.js
+++ src/JS/Buttons/toggleView.js
@@ -6,7 +6,7 @@ toggleButton.addEventListener("click",() => {
   if(productGrid.classList.contains("hidden")) {
     // Switch to grid view
     productGrid.classList.remove("hidden");
-    productCardList.classList.remove("hidden");
+    productCardList.classList.add("hidden");
     toggleButton.textContent="Switch to List View";
 
     // Ensure cards in the grid view are visible
@@ -17,7 +17,7 @@ toggleButton.addEventListener("click",() => {
   } else {
     // Switch to list view
     productGrid.classList.add("hidden");
-    productCardList.classList.add("hidden");
+    productCardList.classList.remove("hidden");
     toggleButton.textContent="Switch to Grid View";
 
     // Ensure cards in the grid view are hidden
