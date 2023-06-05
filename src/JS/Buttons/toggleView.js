4728146d376b33c1eff9d8b7439d188ba8fc8ef0
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-05 15:30:24 -0700

    Update toggleView.js

--- src/JS/Buttons/toggleView.js
+++ src/JS/Buttons/toggleView.js
@@ -1,28 +1,29 @@
 const toggleButton=document.getElementById("toggleButton");
 const productGrid=document.getElementById("productGrid");
 const productCardList=document.getElementById("productCardList");
+
 toggleButton.addEventListener("click",() => {
   if(productGrid.classList.contains("hidden")) {
     // Switch to grid view
     productGrid.classList.remove("hidden");
     productCardList.classList.add("hidden");
     toggleButton.textContent="Switch to List View";
+
     // Ensure cards in the grid view are visible
     const cards=document.querySelectorAll(".card");
     for(let i=0;i<cards.length;i++) {
-      cards[i
-      ].classList.remove("hidden");
+      cards[i].classList.remove("hidden");
     }
   } else {
     // Switch to list view
     productGrid.classList.add("hidden");
     productCardList.classList.remove("hidden");
     toggleButton.textContent="Switch to Grid View";
+
     // Ensure cards in the grid view are hidden
     const cards=document.querySelectorAll(".card");
     for(let i=0;i<cards.length;i++) {
-      cards[i
-      ].classList.add("hidden");
+      cards[i].classList.add("hidden");
     }
   }
 });
