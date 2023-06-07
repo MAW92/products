d36f807fe3aa5aecedc270cf053458c026b6db07
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-06 19:44:56 -0700

    Update populateProductCard.js

--- src/JS/populateProductCard.js
+++ src/JS/populateProductCard.js
@@ -32,9 +32,9 @@ function fetchProducts() {
 
 // Function to create product cards
 function createProductCard(product) {
-  const card=document.createElement("div");
-  card.innerHTML=`
-    <a href="${product.viewItemURL}" target="_blank" title="View on eBay" class="card mt-2 hover:text-shadow text-white">
+  const gridCard=document.createElement("div");
+  gridCard.innerHTML=`
+    <a href="${product.viewItemURL}" target="_blank" title="View on eBay" class="gridCard mt-2 hover:text-shadow text-white">
       <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis will-change-transform hover:transform-gpu hover:duration-500 hover:ease-in-out hover:scale-105 hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
         <img src="${product.galleryURL}" alt="${product.title}" class="w-full h-72 object-cover object-top rounded-t-lg">
         <div class="p-2 flex-wrap">
@@ -46,7 +46,25 @@ function createProductCard(product) {
       </div>
     </a>
   `;
-  return card;
+  return gridCard;
+}
+
+function createProductListCard(product) {
+  const listCard=document.createElement("div");
+  listCard.innerHTML=`
+    <a href="${product.viewItemURL}" target="_blank" title="View on eBay" class="listCard mt-2 hover:text-shadow text-white">
+      <div class="w-fit border border-zinc-50 backdrop rounded-lg shadow-lg overflow-ellipsis hover:bg-gradient-to-b hover:from-transparent hover:to-transparent hover:via-black hover:text-shadow text-white">
+        <img src="${product.galleryURL}" alt="${product.title}" class="w-full h-72 object-cover object-top rounded-t-lg">
+        <div class="p-2 inline-flex flex-wrap">
+          <h3 class="text-lg font-black text-shadow text-white">${product.title}</h3>
+          <p class="mt-2 text-shadow font-bold text-white">$${product.Price}</p>
+          <p class="text-shadow text-sm font-bold text-white">Shipping: $${product.Shipping}</p>
+          <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${product.Condition} condition, kept sleeved and stored in a safe environment.</p>
+        </div>
+      </div>
+    </a>
+  `;
+  return listCard;
 }
 
 // Function to display product cards
