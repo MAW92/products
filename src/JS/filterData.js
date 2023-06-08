b335dff9155a407163aa0ae38a218bc54be7a2b3
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-08 05:19:44 -0700

    Update filterData.js

--- src/JS/filterData.js
+++ src/JS/filterData.js
@@ -5,7 +5,7 @@ function parseJSON(jsonData) {
   const rarity=[];
   const cardCondition=[];
 
-  parsedData[jsonData].ItemSpecifics.NameValueList.forEach((item) => {
+  parsedData[features,rarity,cardCondition].ItemSpecifics.NameValueList.forEach((item) => {
     if(item.Name==="Features") {
       features.push(item.Value);
     } else if(item.Name==="Rarity") {
