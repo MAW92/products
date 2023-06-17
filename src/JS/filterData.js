aff45c87ec0e710651c074be93bea5e59a3aab65
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-17 09:25:52 -0700

    Update filterData.js

--- src/JS/filterData.js
+++ src/JS/filterData.js
@@ -4924,7 +4924,7 @@ function renderItems() {
             <h3 class="text-lg font-black text-shadow text-white">${card.Name}</h3>
             <p class="mt-2 text-shadow font-bold text-white">$${card.Price}</p>
             <p class="text-shadow text-sm font-bold text-white">Shipping: $${card.Shipping}</p>
-            <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${item.ItemSpecifics.NameValueList.find((spec) => spec.Name==='Card Condition').Value} condition, kept sleeved and stored in a safe environment.</p>
+            <p class="mt-5 text-sm italic font-semibold text-shadow text-white">${item.ItemSpecifics.NameValueList.find((spec) => spec.Name==='Card Condition')} condition, kept sleeved and stored in a safe environment.</p>
           </div>
         </div>
         </a>
