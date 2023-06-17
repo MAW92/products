1b8aeca37334cf1e86004e9d212df7096ab219aa
gottasellemall69 <45414617+gottasellemall69@users.noreply.github.com>
2023-06-17 08:34:52 -0700

    Update applyFilters.js

--- src/JS/Buttons/applyFilters.js
+++ src/JS/Buttons/applyFilters.js
@@ -1,7 +1,7 @@
 function applyFilters() {
-  const condition=document.getElementById("conditionFilter").value;
-  const printing=document.getElementById("printingFilter").value;
-  const rarity=document.getElementById("rarityFilter").value;
+  const condition=document.getElementById("condition-filter").value;
+  const printing=document.getElementById("features-filter").value;
+  const rarity=document.getElementById("rarity-filter").value;
 
   console.log("Condition:",condition);
   console.log("Printing:",printing);
